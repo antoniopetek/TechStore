b02bd9c9b6995c51f3306c997cc8c74ebb161df2
Josip Marijanović <jmarijano@foi.hr>
2018-06-12 20:27:20 +0200

    Dodani komentari na metode u formi uiNabava. Dodan konstruktor bez parametara. Dodana metoda IspravnostKolicine. Složena funkcionalnost nabave artikala za pojedinu poslovnicu.

--- TechStore/TechStore/uiNabava.Designer.cs
+++ TechStore/TechStore/uiNabava.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TechStore
 {
-    partial class uiNabava
+    partial class UiNabava
     {
         /// <summary>
         /// Required designer variable.
@@ -28,22 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.uiInputPoslovnica = new System.Windows.Forms.ComboBox();
+            this.uiInputArtikl = new System.Windows.Forms.ComboBox();
+            this.uiInputKolicina = new System.Windows.Forms.TextBox();
+            this.uiActionSpremi = new System.Windows.Forms.Button();
+            this.uiActionOdustani = new System.Windows.Forms.Button();
+            this.poslovnicaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.artiklBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.poslovnicaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artiklBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // FrmNabava
+            // uiInputPoslovnica
+            // 
+            this.uiInputPoslovnica.DataSource = this.poslovnicaBindingSource;
+            this.uiInputPoslovnica.DisplayMember = "Naziv";
+            this.uiInputPoslovnica.FormattingEnabled = true;
+            this.uiInputPoslovnica.Location = new System.Drawing.Point(92, 33);
+            this.uiInputPoslovnica.Name = "uiInputPoslovnica";
+            this.uiInputPoslovnica.Size = new System.Drawing.Size(121, 21);
+            this.uiInputPoslovnica.TabIndex = 0;
+            this.uiInputPoslovnica.ValueMember = "ID";
+            // 
+            // uiInputArtikl
+            // 
+            this.uiInputArtikl.DataSource = this.artiklBindingSource;
+            this.uiInputArtikl.DisplayMember = "Naziv";
+            this.uiInputArtikl.FormattingEnabled = true;
+            this.uiInputArtikl.Location = new System.Drawing.Point(92, 60);
+            this.uiInputArtikl.Name = "uiInputArtikl";
+            this.uiInputArtikl.Size = new System.Drawing.Size(121, 21);
+            this.uiInputArtikl.TabIndex = 1;
+            this.uiInputArtikl.ValueMember = "ID";
+            // 
+            // uiInputKolicina
+            // 
+            this.uiInputKolicina.Location = new System.Drawing.Point(92, 87);
+            this.uiInputKolicina.Name = "uiInputKolicina";
+            this.uiInputKolicina.Size = new System.Drawing.Size(121, 20);
+            this.uiInputKolicina.TabIndex = 2;
+            // 
+            // uiActionSpremi
+            // 
+            this.uiActionSpremi.Location = new System.Drawing.Point(75, 113);
+            this.uiActionSpremi.Name = "uiActionSpremi";
+            this.uiActionSpremi.Size = new System.Drawing.Size(75, 23);
+            this.uiActionSpremi.TabIndex = 3;
+            this.uiActionSpremi.Text = "Spremi";
+            this.uiActionSpremi.UseVisualStyleBackColor = true;
+            this.uiActionSpremi.Click += new System.EventHandler(this.UiActionSpremi_Click);
+            // 
+            // uiActionOdustani
+            // 
+            this.uiActionOdustani.Location = new System.Drawing.Point(156, 113);
+            this.uiActionOdustani.Name = "uiActionOdustani";
+            this.uiActionOdustani.Size = new System.Drawing.Size(75, 23);
+            this.uiActionOdustani.TabIndex = 4;
+            this.uiActionOdustani.Text = "Odustani";
+            this.uiActionOdustani.UseVisualStyleBackColor = true;
+            this.uiActionOdustani.Click += new System.EventHandler(this.UiActionOdustani_Click);
+            // 
+            // poslovnicaBindingSource
+            // 
+            this.poslovnicaBindingSource.DataSource = typeof(TechStore.Poslovnica);
+            // 
+            // artiklBindingSource
+            // 
+            this.artiklBindingSource.DataSource = typeof(TechStore.Artikl);
+            // 
+            // UiNabava
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::TechStore.Properties.Resources.pozadina;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "FrmNabava";
+            this.ClientSize = new System.Drawing.Size(303, 168);
+            this.Controls.Add(this.uiActionOdustani);
+            this.Controls.Add(this.uiActionSpremi);
+            this.Controls.Add(this.uiInputKolicina);
+            this.Controls.Add(this.uiInputArtikl);
+            this.Controls.Add(this.uiInputPoslovnica);
+            this.Name = "UiNabava";
             this.Text = "FrmNabava";
             this.Load += new System.EventHandler(this.FrmNabava_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.poslovnicaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.artiklBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox uiInputPoslovnica;
+        private System.Windows.Forms.ComboBox uiInputArtikl;
+        private System.Windows.Forms.TextBox uiInputKolicina;
+        private System.Windows.Forms.Button uiActionSpremi;
+        private System.Windows.Forms.Button uiActionOdustani;
+        private System.Windows.Forms.BindingSource poslovnicaBindingSource;
+        private System.Windows.Forms.BindingSource artiklBindingSource;
     }
 }
--- TechStore/TechStore/uiNabava.cs
+++ TechStore/TechStore/uiNabava.cs
@@ -10,19 +10,60 @@ using System.Windows.Forms;
 
 namespace TechStore
 {
-    public partial class uiNabava : Form
+    public partial class UiNabava : Form
     {
-        public uiNabava()
+        private int artiklNabavaId;
+        private Poslovnica poslovnicaNabava;
+        private string artiklNabavaNaziv;
+
+        /// <summary>
+        /// Konstruktor forme uiNabava
+        /// </summary>
+        public UiNabava()
         {
             InitializeComponent();
         }
+        /// <summary>
+        /// Konstruktor forme uiNabava. Kao argumente prima artikl
+        /// koji želimo naručiti te poslovnicu za koju želimo naručiti
+        /// artikl.
+        /// </summary>
+        public UiNabava(int artikl, Poslovnica poslovnica, string artiklNaziv)
+        {
+            InitializeComponent();
+            artiklNabavaId = artikl;
+            poslovnicaNabava = poslovnica;
+            artiklNabavaNaziv = artiklNaziv;
+        }
 
+        /// <summary>
+        /// Metoda koja se poziva prilikom učitavanja forme uiNabava
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FrmNabava_Load(object sender, EventArgs e)
         {
             this.KeyPreview = true;
             this.KeyDown += FrmNabava_KeyDown;
+            if (poslovnicaNabava == null || artiklNabavaId == 0 || artiklNabavaNaziv == "")
+            {
+                artiklBindingSource.DataSource = Artikl.DohvatiSveArtikle();
+                poslovnicaBindingSource.DataSource = Poslovnica.DohvatiPoslovnice();
+            }
+            else
+            {
+                uiInputArtikl.Text = artiklNabavaNaziv.ToString();
+                uiInputPoslovnica.Text = poslovnicaNabava.Naziv;
+                uiInputArtikl.Enabled = false;
+                uiInputPoslovnica.Enabled = false;
+            }
         }
 
+        /// <summary>
+        /// Metoda koja se poziva prilikom pritiska tipke F11
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void FrmNabava_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode.ToString() == "F1")
@@ -35,6 +76,88 @@ namespace TechStore
                 frmHelp.Show();
 
             }
+        }
+
+        /// <summary>
+        /// Metoda koja se poziva kod pritiska na gumbić Odustani.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiActionOdustani_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Metoda koja se poziva kod pristika na gumbić Spremi.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiActionSpremi_Click(object sender, EventArgs e)
+        {
+            if (poslovnicaNabava == null || artiklNabavaId == 0 || artiklNabavaNaziv == "")
+            {
+                if (IspravnostKolicine())
+                {
+                    Poslovnica poslovnicaIzComboBoxa = (Poslovnica)poslovnicaBindingSource.Current;
+                    Dostupnost postojeca = Dostupnost.DohvatiDostupnost(poslovnicaIzComboBoxa, int.Parse(uiInputArtikl.SelectedValue.ToString()));
+                    if (postojeca==null)
+                    {
+                        Dostupnost novaDostupnost = new Dostupnost
+                        {
+                            Artikl_ID = int.Parse(uiInputArtikl.SelectedValue.ToString()),
+                            Poslovnica_ID = int.Parse(uiInputPoslovnica.SelectedValue.ToString()),
+                            Kolicina = int.Parse(uiInputKolicina.Text)
+                        };
+                        Dostupnost.DodajDostupnost(novaDostupnost);
+                        MessageBox.Show("Uspješno je naručen proizvod za poslovnicu !", "Naručen artikl!", MessageBoxButtons.OK);
+                    }
+                    else
+                    {
+                        Dostupnost.IzmjenaDostupnosti(postojeca, int.Parse(uiInputKolicina.Text));
+                        MessageBox.Show("Uspješno je naručen proizvod za poslovnicu !", "Naručen artikl!", MessageBoxButtons.OK);
+                    }
+                    
+                    this.Close();
+                }
+                else
+                {
+                    
+                    MessageBox.Show("Unesite pozitivan broj za količinu", "GREŠKA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                if (IspravnostKolicine())
+                {
+                    Dostupnost dostupnost = Dostupnost.DohvatiDostupnost(poslovnicaNabava, artiklNabavaId);
+                    Dostupnost.IzmjenaDostupnosti(dostupnost, int.Parse(uiInputKolicina.Text));
+                    MessageBox.Show("Uspješno je naručen proizvod " + artiklNabavaNaziv + " za poslovnicu " + poslovnicaNabava.Naziv + "!", "Naručen artikl!", MessageBoxButtons.OK);
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Unesite pozitivan broj za količinu", "GREŠKA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+
+        /// <summary>
+        /// Metoda koja provjerava ispravnost unesene količine.
+        /// </summary>
+        /// <returns></returns>
+        private bool IspravnostKolicine()
+        {
+            if (int.TryParse(uiInputKolicina.Text, out int kolicina))
+            {
+                if (kolicina > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
         }
     }
 }
