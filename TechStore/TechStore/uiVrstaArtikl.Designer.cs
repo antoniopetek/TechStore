bebf2b6bd4975221ce264e8dd2dda76e9dd35421
Josip Marijanović <jmarijano@foi.hr>
2018-06-12 11:17:06 +0200

    Dodane forme uiDodavanjeVrsteArtikla te euiVrstaArtikla.

---
+++ TechStore/TechStore/uiVrstaArtikl.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace TechStore
+{
+    partial class UiVrstaArtikl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nazivDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.artiklDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vrstaArtiklaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.uiActionDodaj = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vrstaArtiklaBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.nazivDataGridViewTextBoxColumn,
+            this.artiklDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.vrstaArtiklaBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(719, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // nazivDataGridViewTextBoxColumn
+            // 
+            this.nazivDataGridViewTextBoxColumn.DataPropertyName = "Naziv";
+            this.nazivDataGridViewTextBoxColumn.HeaderText = "Naziv";
+            this.nazivDataGridViewTextBoxColumn.Name = "nazivDataGridViewTextBoxColumn";
+            // 
+            // artiklDataGridViewTextBoxColumn
+            // 
+            this.artiklDataGridViewTextBoxColumn.DataPropertyName = "Artikl";
+            this.artiklDataGridViewTextBoxColumn.HeaderText = "Artikl";
+            this.artiklDataGridViewTextBoxColumn.Name = "artiklDataGridViewTextBoxColumn";
+            this.artiklDataGridViewTextBoxColumn.Visible = false;
+            // 
+            // vrstaArtiklaBindingSource
+            // 
+            this.vrstaArtiklaBindingSource.DataSource = typeof(TechStore.VrstaArtikla);
+            // 
+            // uiActionDodaj
+            // 
+            this.uiActionDodaj.Location = new System.Drawing.Point(656, 168);
+            this.uiActionDodaj.Name = "uiActionDodaj";
+            this.uiActionDodaj.Size = new System.Drawing.Size(75, 23);
+            this.uiActionDodaj.TabIndex = 1;
+            this.uiActionDodaj.Text = "Dodaj";
+            this.uiActionDodaj.UseVisualStyleBackColor = true;
+            this.uiActionDodaj.Click += new System.EventHandler(this.UiActionDodaj_Click);
+            // 
+            // UiVrstaArtikl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::TechStore.Properties.Resources.pozadina;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.uiActionDodaj);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "UiVrstaArtikl";
+            this.Text = "uiVrstaArtikl";
+            this.Load += new System.EventHandler(this.UiVrstaArtikl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vrstaArtiklaBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nazivDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn artiklDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource vrstaArtiklaBindingSource;
+        private System.Windows.Forms.Button uiActionDodaj;
+    }
+}
---
+++ TechStore/TechStore/uiVrstaArtikl.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechStore
+{
+    public partial class UiVrstaArtikl : Form
+    {
+        /// <summary>
+        /// Konstruktor forme uiVrstaArtikl
+        /// </summary>
+        public UiVrstaArtikl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Metoda koja se poziva prilikom učitavanja
+        /// forme uiVrstaArtikl
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiVrstaArtikl_Load(object sender, EventArgs e)
+        {
+            vrstaArtiklaBindingSource.DataSource = VrstaArtikla.DohvatiVrsteArtikala();
+        }
+
+        /// <summary>
+        /// Metoda koja se poziva prilikom pritiska na gumbić Dodaj.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UiActionDodaj_Click(object sender, EventArgs e)
+        {
+            vrstaArtiklaBindingSource.DataSource = VrstaArtikla.DohvatiVrsteArtikala();
+        }
+    }
+}
