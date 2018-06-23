56284f06e6b450cada1092b4569d2e9b147f2d42
Antonio Petek <apetek@foi.hr>
2018-06-23 18:15:53 +0200

    Napravljena testni projekt TechStoreTest. Napravljene dvije testne klase ZaposlenikTest i PoslovnicaTest. U testnoj klasi ZaposlenikTest napravljene dvije metode DohvatiZaposlenikaTest_ZaposlenikPostoji() i DohvatiZaposlenikaTest_ZaposlenikNePostoji(). U testnoj klasi PoslovnicaTest napravljene dvije metode DohvatiPoslovnicuTest_PoslovnicaPostoji() i DohvatiPoslovnicuTest_PoslovnicaNePostoji().

---
+++ TechStore/TechStoreTest/PoslovnicaTest.cs
@@ -0,0 +1,24 @@
+﻿using TechStore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace TechStoreTest
+{
+    [TestClass]
+    public class PoslovnicaTest
+    {
+        [TestMethod]
+        public void DohvatiPoslovnicuTest_PoslovnicaPostoji()
+        {
+            Poslovnica poslovnica = null;
+            poslovnica = Poslovnica.DohvatiPoslovnicu(1);
+            Assert.IsNotNull(poslovnica);
+        }
+
+        [TestMethod]
+        public void DohvatiPoslovnicuTest_PoslovnicaNePostoji()
+        {
+            Poslovnica poslovnica = null;
+            poslovnica = Poslovnica.DohvatiPoslovnicu(99);
+            Assert.IsNull(poslovnica);
+        }
+    }
+}
---
+++ TechStore/TechStoreTest/ZaposlenikTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechStore;
+
+namespace TechStoreTest
+{
+    [TestClass]
+    public class ZaposlenikTest
+    {
+        [TestMethod]
+        public void DohvatiZaposlenikaTest_ZaposlenikPostoji()
+        {
+            Zaposlenik zaposlenik = null;
+            zaposlenik = Zaposlenik.DohvatiZaposlenika("apetek", "password");
+            Assert.IsNotNull(zaposlenik);
+        }
+
+        [TestMethod]
+        public void DohvatiZaposlenikaTest_ZaposlenikNePostoji()
+        {
+            Zaposlenik zaposlenik = null;
+            zaposlenik = Zaposlenik.DohvatiZaposlenika("marulic", "marko");
+            Assert.IsNull(zaposlenik);
+        }
+    }
+}
