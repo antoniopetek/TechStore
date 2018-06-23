d5e48fb038795c85994e7029e99f7862247f3b8a
Antonio Petek <apetek@foi.hr>
2018-06-23 20:38:31 +0200

    Napravljena odvojena komponenta (.dll) Komponente. U komponenti napravljena metoda za pretraživanje DataGridView-a.

---
+++ TechStore/Komponente/Pretraga.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Komponente
+{
+    public class Pretraga
+    {
+        public static void Pretrazi(DataGridView dgv, string tekst, int kolona)
+        {
+            foreach (DataGridViewRow red in dgv.Rows)
+            {
+                if (red.Cells[kolona].Value.ToString().ToLower().Contains(tekst))
+                {
+                    red.Visible = true;
+                }
+                else
+                {
+                    CurrencyManager currencyManager = (CurrencyManager)dgv.BindingContext[dgv.DataSource];
+                    currencyManager.SuspendBinding();
+                    red.Visible = false;
+                    currencyManager.ResumeBinding();
+                }
+            }
+        }
+    }
+}
